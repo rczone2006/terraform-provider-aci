7cf2143091f9c79087978fe1655777edafc3d285
rczone2006 <rczone2006@gmail.com>
2021-05-26 14:30:08 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rczone2006/terraform_provider_aci
+
+go 1.16
+
+require github.com/hashicorp/terraform-plugin-sdk v1.17.2 // indirect
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/terraform-providers/terraform-provider-aci/aci"
+//	"github.com/terraform-providers/terraform-provider-aci/aci"
 )
 
 func main() {
